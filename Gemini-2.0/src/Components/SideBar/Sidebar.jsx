1455d0dafcdb69359401116c4429acde69e13771
PrathamArora20 <apratham2421@gmail.com>
2025-01-04 20:50:04 -0700

    sidebar done

--- Gemini-2.0/src/Components/SideBar/Sidebar.jsx
+++ Gemini-2.0/src/Components/SideBar/Sidebar.jsx
@@ -4,43 +4,49 @@ import { IoMenuOutline } from "react-icons/io5";
 import { CiSquarePlus } from "react-icons/ci";
 import { FaRegMessage } from "react-icons/fa6";
 import { CiSquareQuestion } from "react-icons/ci";
-import { FaHistory } from "react-icons/fa";
+import { MdHistory } from "react-icons/md";
 import { CiSettings } from "react-icons/ci";
 
 
 export const Sidebar = () => {
+
+    const [extended, setExtended] = React.useState(false)
   return (
     <div className='sidebar'>
         <div className="top">
-            <IoMenuOutline className='menu' size={24} color="#000" />
-            <div className="new-chat">
-                <CiSquarePlus size={24} color="#000" />
-                <p>New Chat</p>
+            <div className="new-chat-button">
+                <IoMenuOutline onClick={()=>setExtended(prev=>!prev)} className='menu' size={24} color="#000" />
+                <div className="new-chat">
+                    <CiSquarePlus size={24} color="#000" />
+                    {extended?<p>New Chat</p>:null}
+                </div>
             </div>
+            {extended?
             <div className="recent">
                 <p className="recent-title">
                     Recent
                 </p>
                 <div className="recent-entry">
-                    <FaRegMessage size={24} color="#000" />
-                </div>
-                <p>
-                    What is react..
-                </p>
+                    <FaRegMessage size={15} color="#000" />
+                    <p>
+                        What is react..
+                    </p>
+                </div>    
             </div>
+            :null}
         </div>
         <div className="bottom">
             <div className="bottom-item recent-entry">
                 <CiSquareQuestion size={24} color="#000" />
-                <p>Help</p>
+                {extended?<p>Help</p>:null}
             </div>
             <div className="bottom-item recent-entry">
-                <FaHistory size={24} color="#000" />
-                <p>Activity</p>
+                <MdHistory size={24} color="#000" />
+                {extended?<p>Activity</p>:null}
             </div>
             <div className="bottom-item recent-entry">
                 <CiSettings size={24} color="#000" />
-                <p>Setting</p>
+                {extended?<p>Setting</p>:null}
             </div>
         </div>
     </div>
