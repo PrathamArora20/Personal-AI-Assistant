b85dd81acd38c870a3030352910d2a39ef9faae9
PrathamArora20 <apratham2421@gmail.com>
2025-01-05 21:56:57 -0700

    Incoporated responses from gemini

--- AI-Personal-Assistant/src/Components/Main/Main.jsx
+++ AI-Personal-Assistant/src/Components/Main/Main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Main.css'
-import { FaRegUser } from "react-icons/fa";
+import { FaRegUser, FaUser } from "react-icons/fa";
 import { FaRegCompass } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
-
+import { Context } from '../../context/Context';
+import { FaRobot } from "react-icons/fa";
 
 export const Main = () => {
+
+    const {onSent,recentPrompt,results,input,setInput,resultData}=React.useContext(Context)
+
   return (
     <div className="main">
         <div className="nav">
@@ -13,33 +17,47 @@ export const Main = () => {
             <FaRegUser size={24} color="#000" />
         </div>
         <div className="main-container">
-            <div className="greating">
-                <p><span>Hello!</span></p>
-                <p>How can I assist you today?</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                    <p>What is the distance between earth to moon</p>
-                    <FaRegCompass className="Compass" size={35} color="#000" />
+            {!results?
+            <>
+                <div className="greating">
+                    <p><span>Hello!</span></p>
+                    <p>How can I assist you today?</p>
                 </div>
-                <div className="card">
-                    <p>Suggest a good place to visit in europe</p>
-                    <FaRegCompass className="Compass" size={35} color="#000" />
+                <div className="cards">
+                    <div className="card">
+                        <p>What is the distance between earth to moon</p>
+                        <FaRegCompass className="Compass" size={35} color="#000" />
+                    </div>
+                    <div className="card">
+                        <p>Suggest a good place to visit in europe</p>
+                        <FaRegCompass className="Compass" size={35} color="#000" />
+                    </div>
+                    <div className="card">
+                        <p>Explain the process of evolution</p>
+                        <FaRegCompass className="Compass" size={35} color="#000" />
+                    </div>
+                    <div className="card">
+                        <p>Write me a code block for inverting a list</p>
+                        <FaRegCompass className="Compass" size={35} color="#000" />
+                    </div>
                 </div>
-                <div className="card">
-                    <p>Explain the process of evolution</p>
-                    <FaRegCompass className="Compass" size={35} color="#000" />
+            
+            </>:<div className="result">
+                <div className="result-title">
+                    <FaRegUser className='UserImage' color="#000" />
+                    <p>{recentPrompt}</p>
                 </div>
-                <div className="card">
-                    <p>Write me a code block for inverting a list</p>
-                    <FaRegCompass className="Compass" size={35} color="#000" />
+                <div className="result-data">
+                    <FaRobot className='robotImage'  color="#000" />
+                    <p dangerouslySetInnerHTML={{__html:resultData}}></p>
                 </div>
-            </div>
+                </div>}
+
             <div className="bottom-main">
                 <div className="search">
-                    <input type="text" placeholder="Search" />
+                    <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Search" />
                     <div>
-                        <FaSearch className="searchImg" size={20} color="#000" />
+                        <FaSearch onClick={()=>onSent()} className="searchImg" size={20} color="#000" />
                     </div>
                 </div>
             </div>
--- AI-Personal-Assistant/src/context/Context.jsx
+++ AI-Personal-Assistant/src/context/Context.jsx
@@ -1,17 +1,48 @@
+import React from 'react'
 import { createContext } from "react";
 import run from "../config/gemini";
+
 export const Context=createContext();
 
 const ContextProvider=(props)=>{
 
+    const [input, setInput] = React.useState('');
+    const [recentPrompt, setRecentPrompt] = React.useState('');
+    const [previousPrompts, setPreviousPrompts] = React.useState([]);
+    const [results, setResults] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    const [resultData, setResultData] = React.useState("");
 
-    const onSent = async(prompt)=>{
-        await run(prompt)
+
+
+    const onSent = async()=>{
+        setResultData('')
+        setLoading(true)
+        setResults(true)
+        setRecentPrompt(input)
+        const response=await run(input)
+        setResultData(response)
+        setLoading(false)
+        setInput('')
+
     }
 
-    onSent("what is react js")
 
-    const contextValue={}
+    const contextValue={
+        previousPrompts,
+        setPreviousPrompts,
+        onSent,
+        recentPrompt,
+        setRecentPrompt,
+        resultData,
+        setResultData,
+        loading,
+        setLoading,
+        results,
+        setResults,
+        input,
+        setInput
+    }
 
 
     return (
